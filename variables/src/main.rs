ed56750c8a225cf209f3c356012b0cc0ecb98229
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2021-09-12 17:20:07 -0500

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    const MAX_POINTS: u32 = 100_000;
+    let x = 6;
+    println!("x is {}", x);
+    let x = 4;
+    let x = x * 2;
+    println!("x is {}", x);
+    println!("max points is {}", MAX_POINTS);
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("spaces is {}", spaces);
+}
